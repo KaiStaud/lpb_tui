37c460a6f5da1a37f3b058b82553576f34c7e2a8
Kai Staud <kai-staud@web.de>
2021-12-17 17:21:19 +0100

    Animations reintegrated

--- textinterface/tui.go
+++ textinterface/tui.go
@@ -9,7 +9,6 @@ package tui
 
 import (
 	"fmt"
-	"strconv"
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -77,8 +76,15 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, tea.Quit
 		}
 	}
-	// Which View needs to be changed?
-	return updateHandler(msg, m)
+	// Handle Selections  and Animations differently:
+
+	// Is A selection made?
+	if !m.Chosen {
+		return updateHandler(msg, m)
+	}
+
+	// There are new animation request:
+	return updateChosen(msg, m)
 }
 
 /* View is preformed after Update to display the changes
@@ -92,8 +98,14 @@ func (m model) View() string {
 
 // Sub-View functions
 func viewHandler(m model) string {
+
+	// Are there any changes in any level?
 	if m.OptionChosen {
-		return choicesView(m)
+		if m.Chosen {
+			return chosenView(m)
+		} else {
+			return choicesView(m)
+		}
 	} else {
 		return terminalOptions(m)
 	}
@@ -142,106 +154,7 @@ func updateHandler(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
 		m.Ticks += 1
 		return m, tick()
 	}
+
+	// Execute Animations after a selection:
 	return m, nil
-}
-
-// Update loop for the first view where you're choosing a task.
-func updateChoices(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-
-	case tea.KeyMsg:
-
-		switch msg.String() {
-		case "j", "down":
-			m.Choice += 1
-			if m.Choice > 3 {
-				m.Choice = 3
-			}
-		case "k", "up":
-			m.Choice -= 1
-			if m.Choice < 0 {
-				m.Choice = 0
-			}
-		case "enter":
-			m.Chosen = true
-			m.Option = 1
-			return m, frame()
-		}
-
-	case tickMsg:
-		m.Ticks += 1
-		return m, tick()
-	}
-
-	return m, nil
-}
-
-// Update loop for the second view after a choice has been made
-func updateChosen(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
-	switch msg.(type) {
-
-	case frameMsg:
-		if !m.Loaded {
-			m.Frames += 1
-			m.Progress += 0.025
-			if m.Progress >= 1 {
-				m.Frames = 0
-				m.Progress = 0
-				m.Loaded = false
-				m.Chosen = false
-				return m, tick()
-			}
-			return m, frame()
-		}
-
-	case tickMsg:
-		if m.Loaded {
-			m.Ticks += 1
-			return m, tick()
-		}
-	}
-
-	return m, nil
-}
-
-// Sub-views
-
-// The first view, where you're choosing a task
-func choicesView(m model) string {
-	c := m.Choice
-
-	tpl := "Select Mode\n\n"
-	tpl += "%s\n\n"
-	tpl += "Up-Time in %s seconds\n\n"
-	tpl += subtle("j/k, up/down: select") + dot + subtle("enter: choose") + dot + subtle("esc: leave menue") + subtle("q: quit")
-
-	choices := fmt.Sprintf(
-		"%s\n%s\n%s\n%s",
-		checkbox("Teaching", c == 0),
-		checkbox("Operation", c == 1),
-		checkbox("Shutdown", c == 2),
-		checkbox("Test", c == 3),
-	)
-
-	return fmt.Sprintf(tpl, choices, colorFg(strconv.Itoa(m.Ticks), "79"))
-}
-
-// The second view, after a task has been chosen
-func chosenView(m model) string {
-	var msg string
-
-	switch m.Choice {
-	case 0:
-		msg = fmt.Sprintf("Changing Mode to %s", keyword("Teaching"))
-	case 1:
-		msg = fmt.Sprintf("Changing Mode to %s", keyword("Teaching"))
-	case 2:
-		msg = fmt.Sprintf("Changing Mode to %s", keyword("Teaching"))
-	default:
-		msg = fmt.Sprintf("Changing Mode to %s. Communication Line will be changed to %s...", keyword("Testing"), keyword("Internal Loopback Mode"))
-	}
-
-	label := "Requesting OpChange..."
-
-	return msg + "\n\n" + label + "\n" + progressbar(80, m.Progress) + "%"
 }
